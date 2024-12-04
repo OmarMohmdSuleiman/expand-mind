9b46afc697e44bcc6e5fecabf19238606abfaf52
Omar <omarsuleiman739@gmail.com>
2024-12-04 18:13:21 +0200

    Delete vit.config.js file

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
-function App(){
-    return (
-        <div><h1>Hello to E-Learning site</h1></div>
-    )
+function App() {
+  return (
+    <div>
+      <h1>Hello to E Learning site</h1>
+    </div>
+  );
 }
 
 export default App;
