e69d894bdc0fd3c9d99aa8e701ac13f3c9a69ea4
Omar <omarsuleiman739@gmail.com>
2024-12-05 18:33:41 +0200

    Import react into the 3 files then export the files to be used later

---
+++ frontend/src/components/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Admin(){
+    return(
+        <div><h1>Admin</h1></div>
+    )
+}
+
+export default Admin;
---
+++ frontend/src/components/InstructorDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Instructor(){
+    return(
+        <div><h1>Instructor</h1></div>
+    )
+}
+
+export default Instructor;
