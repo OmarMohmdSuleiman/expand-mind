ec352b2dfc67c7c5af1b554b6fca3dca5fe80904
Omar <omarsuleiman739@gmail.com>
2024-12-05 18:45:33 +0200

    Import the 3 dashboards to App.jsx and make their routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Login';
 import SignUp from './SignUp';
+import Students from './StudentDashboard';
+import Instructor from './InstructorDashboard';
+import Admin from './AdminDashboard';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path="/student-dashboard" element={<Students />} />
+          <Route path="/instructor-dashboard" element={<Instructor />} />
+          <Route path="/admin-dashboard" element={<Admin />} />
         </Routes>
       </Router>
     </div>
