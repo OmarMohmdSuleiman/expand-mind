41f8a8c94197db977b0c36746060575966810182
Omar <omarsuleiman739@gmail.com>
2024-12-08 11:15:27 +0200

    Untrack .env and update .gitignore

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,15 @@ import pg from "pg";
 const app = express();
 const port = 4000;
 
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+  db.connect();
+
 app.get('/', (req, res) => {
     res.status(200).send("Ready");
 });
