3e24b55507750b6ed3b12f29448a33953078c327
Omar <omarsuleiman739@gmail.com>
2024-12-08 10:54:59 +0200

    Set up basic Express server with PG integration and root route

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-import express 
+import express from "express";
+import pg from "pg";
+
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.status(200).send("Ready");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
