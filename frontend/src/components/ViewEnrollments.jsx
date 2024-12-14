d426f357622c14029704e3ad21e4fc8fc4818b40
Omar <omarsuleiman739@gmail.com>
2024-12-14 19:53:02 +0200

    Add useEffect to fetch enrollments and state management for enrollments in ViewEnrollments.jsx

--- frontend/src/components/ViewEnrollments.jsx
+++ frontend/src/components/ViewEnrollments.jsx
@@ -1,18 +1,22 @@
-import React,{useState} from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import EachEnrollments from "./EachEnrollment";
 
+function ViewEnrollments() {
+  const [enrollments, setEnrollments] = useState([]);
 
-function ViewCourses(){
-    
-  
-    return (
-      <div>
-        <h1>All Courses</h1>
-        <ul>
-          
-        </ul>
-      </div>
-    );
+  useEffect(() => {
+    fetch("http://localhost:4000/enrollments")
+    .then((response) => response.json())
+    .then((data) => setEnrollments(data))
+    .catch((error) => console.error('Error fetching enrollments:', error));
+}, []);
+  return (
+    <div>
+      <h1>All Enrollments</h1>
+      {/* Pass grouped enrollments to EachEnrollments component */}
+      <EachEnrollments enrollments={enrollments} />
+    </div>
+  );
 }
 
-export default ViewCourses;
+export default ViewEnrollments;
