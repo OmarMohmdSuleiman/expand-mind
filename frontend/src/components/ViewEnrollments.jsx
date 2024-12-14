8716b5a90149497a08203e28810e44ede1e3b87d
Omar <omarsuleiman739@gmail.com>
2024-12-14 18:47:40 +0200

    Add some code in ViewEnrollments.jsx

---
+++ frontend/src/components/ViewEnrollments.jsx
@@ -0,0 +1,18 @@
+import React,{useState} from "react";
+import { useEffect } from "react";
+
+
+function ViewCourses(){
+    
+  
+    return (
+      <div>
+        <h1>All Courses</h1>
+        <ul>
+          
+        </ul>
+      </div>
+    );
+}
+
+export default ViewCourses;
