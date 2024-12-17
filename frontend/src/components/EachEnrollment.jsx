2ddd95bdf5bb21d2ce454e2168baee7620ff5063
Omar <omarsuleiman739@gmail.com>
2024-12-17 17:30:21 +0200

    Add the Enrollment component in EachEnrollment.jsx and pass the student's name and the children as a prop

--- frontend/src/components/EachEnrollment.jsx
+++ frontend/src/components/EachEnrollment.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import Enrollment from "./Enrollment";
 
 function EachEnrollments({ enrollments }) {
   return (
     <div>
-      {enrollments.map((enrollment, index) => (
-        <div key={index}>
-          <h2>{enrollment.name}</h2> {/* Display the student's name */}
-          <ul>
-            {enrollment.courses.map((course, courseIndex) => (
-              <li key={courseIndex}>{course}</li>  
-            ))}
-          </ul>
-        </div>
-      ))}
+      <ul>
+        {enrollments.map((enrollment, index) => (
+          <Enrollment key={index} name={enrollment.name}>
+            <ul>
+              {enrollment.courses.map((course, courseIndex) => (
+                <li key={courseIndex}>{course}</li>
+              ))}
+            </ul>
+          </Enrollment>
+        ))}
+      </ul>
     </div>
   );
 }
