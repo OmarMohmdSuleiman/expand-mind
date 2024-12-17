897dce3c702bc7e67a06ae76e8888f7978279f6e
Omar <omarsuleiman739@gmail.com>
2024-12-17 18:20:24 +0200

    Add Route for ViewInstructors component in the main Routes setup in App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -8,6 +8,7 @@ import Admin from './AdminDashboard';
 import ViewCourses from './ViewCourses';
 import ShowStudents from './ShowStudents';
 import ViewEnrollments from './ViewEnrollments';
+import ViewInstructors from './ViewInstructors';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/admin-dashboard/view-courses" element={<ViewCourses />} />
           <Route path="/admin-dashboard/view-students" element={<ShowStudents />} />
           <Route path="/admin-dashboard/view-enrollments" element={<ViewEnrollments />} />
+          <Route path="/admin-dashboard/view-instructors" element={<ViewInstructors />} />
         </Routes>
       </Router>
     </div>
