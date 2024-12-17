fffad6967e17363f339b5afe1a221cb2a6df2b22
Omar <omarsuleiman739@gmail.com>
2024-12-17 18:50:19 +0200

    Add delete button  with red color style in Instructor.jsx

--- frontend/src/components/Instructor.jsx
+++ frontend/src/components/Instructor.jsx
@@ -3,7 +3,11 @@ import React from "react";
 
 function Instructors(props){
     return (<li><h2>{props.name}</h2>
-    <h3>{props.course}</h3></li>);
+    <h3>{props.course}</h3>
+    <button onClick={props.onDelete} style={{ color: "red" }}>
+        Delete
+      </button></li>
+    );
 }
 
 export default Instructors;
