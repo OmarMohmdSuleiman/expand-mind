7f0c055e68275a385f748c0f0c6c929237650b3c
Omar <omarsuleiman739@gmail.com>
2024-12-31 19:46:36 +0200

    Create Header.jsx to add log out btn

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header(){
+    return(
+        <div className="header">
+            <div className="project_name">Expandmind</div>
+            <button className="logout">Logout</button>
+        </div>
+    )
+}
+export default Header;
